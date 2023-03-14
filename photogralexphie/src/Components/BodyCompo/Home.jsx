95b5df722f433af64e6b917b5b9fa838108870c1
Jordy Rocacher <jmr4277@orange.fr>
2023-03-14 20:59:04 +0100

    home page in work, learn react-spring

--- photogralexphie/src/Components/BodyCompo/Home.jsx
+++ photogralexphie/src/Components/BodyCompo/Home.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { ArrayImg } from "./HomeImg/ArrayImg";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+const image = require("../BodyCompo/HomeImg/Section_animal_phone.jpg");
 
 export const Home = () => {
   return (
-    <div className="home-img-container">
-      {ArrayImg.map((item, index) => {
-        return <div key={index} className={item.class}></div>;
-      })}
+    <div>
+      <Parallax pages={2.28} className="home-img-container" style={{}}>
+        {ArrayImg.map((item, index) => {
+          return (
+            <ParallaxLayer
+              key={index}
+              offset={item.offset}
+              speed={0.5}
+              className={item.class}
+              style={item.style}
+            >
+              <ParallaxLayer speed={0.2}>
+                <h2 style={item.marge}>{item.title}</h2>
+              </ParallaxLayer>
+            </ParallaxLayer>
+          );
+        })}
+      </Parallax>
     </div>
   );
 };
--- photogralexphie/src/Components/BodyCompo/HomeImg/ArrayImg.jsx
+++ photogralexphie/src/Components/BodyCompo/HomeImg/ArrayImg.jsx
@@ -1,6 +1,52 @@
+const image1 = require("../../BodyCompo/HomeImg/Section_animal_phone.jpg");
+const image2 = require("../../BodyCompo/HomeImg/Section_seul.jpg");
+const image3 = require("../../BodyCompo/HomeImg/Section_couple_phone.jpg");
+const image4 = require("../../BodyCompo/HomeImg/Section_event_phone.jpg");
+
 export const ArrayImg = [
-  { title: "Animaux", class: "home-img section-animals" },
-  { title: "Portrait Seul", class: "home-img section-solo" },
-  { title: "Portrait Couple", class: "home-img section-couple" },
-  { title: "Portrait Couple", class: "home-img section-event" },
+  {
+    title: "Animaux",
+    class: "home-img section-animals",
+    src: require("./Section_animal_phone.jpg"),
+    offset: 0,
+    style: {
+      backgroundImage: `url(${image1})`,
+      backgroundSize: "395px 600px",
+      height: "72vh",
+    },
+    marge: { margin: "500px 150px" },
+  },
+  {
+    title: "Portrait Seul",
+    class: "home-img section-solo",
+    src: require("./Section_seul.jpg"),
+    offset: 0.71,
+    style: {
+      backgroundImage: `url(${image2})`,
+      backgroundSize: "395px 700px",
+      height: "80vh",
+    },
+  },
+  {
+    title: "Portrait Couple",
+    class: "home-img section-couple",
+    src: require("./Section_couple_phone.jpg"),
+    offset: 1,
+    style: {
+      backgroundImage: `url(${image3})`,
+      backgroundSize: "395px 600px",
+      height: "72vh",
+    },
+  },
+  {
+    title: "Portrait Evennement",
+    class: "home-img section-event",
+    src: require("./Section_event_phone.jpg"),
+    offset: 1.7,
+    style: {
+      backgroundImage: `url(${image4})`,
+      backgroundSize: "395px 700px",
+      height: "72vh",
+    },
+  },
 ];
